e08bafd279ba0015d7b652789cec66117fd73090
David <DavidGranados@fullentregas.com.ar>
2013-10-21 17:36:40 -0300

    12

--- Contacto.aspx.vb
+++ Contacto.aspx.vb
@@ -9,13 +9,4 @@ Partial Class Contacto
     Protected Sub img_Twitter_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles img_Twitter.Click
         Response.Redirect("https://twitter.com/Full_SRL")
     End Sub
-
-
-    Protected Sub img_Linkedin_Click(ByVal sender As Object, ByVal e As System.Web.UI.ImageClickEventArgs) Handles img_Linkedin.Click
-<<<<<<< HEAD
-        Response.Redirect("")
-=======
-        Response.Redirect("#")
->>>>>>> 5a59ee1460cedb6f0a5320220ca8be5f8f3751d3
-    End Sub
 End Class
