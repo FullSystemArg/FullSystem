c96c368ae7f92bd9b432ab100fb1a7b32bad7bba
David <DavidGranados@fullentregas.com.ar>
2013-10-21 17:24:32 -0300

    Master

--- Site.master.vb
+++ Site.master.vb
@@ -4,7 +4,7 @@ Partial Class Site
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not IsPostBack Then
-
+            'inicio
         End If
     End Sub
 
